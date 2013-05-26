12ed7f3e4acd3d8a9af7c69fd7ee0275fe6afe59
Leo Bergnéhr <leo@bergnehr.se>
2013-05-25 23:59:32 -0400

    Add more complete range of unicode characters

--- insert_character.rb
+++ insert_character.rb
@@ -3,16 +3,23 @@ require 'osx/cocoa'
 include OSX
 
 # Get and check input values
-input = "{query}".split " "
+input = "snow".split " "
 if input == nil || input.length == 0
   return
 end
 
-MIN_CHAR = 32
-MAX_CHAR = 2 ** 16
+intervals = [
+  0..0x2ffff,
+  0xe0000..0xeffff
+]
+
+# MIN_CHAR = 32
+# MAX_CHAR = 2 ** 16
+
+range = intervals.map { |x| x.to_a } .flatten
 
 # Loop over "all" unicode characters
-(MIN_CHAR..MAX_CHAR).each do |i|
+range.each do |i|
 
   # Create a unicode string from this number/byte
   name = OSX::NSMutableString.stringWithFormat "%C", i
@@ -24,13 +31,12 @@ MAX_CHAR = 2 ** 16
     # Transforming the unicode character to its unicode name
     success = OSX::CFStringTransform name, nil, ")kCFStringTransformToUnicodeName", false
 
-    if success && name != nil && name != ""
-      name = name.to_str.downcase
+if success && name != nil && name != ""
+  name = name.to_str.downcase
       # If the unicode name matches any word in our query, print it
       if input.any? { |q| name.include? q.downcase }
         puts char
       end
-
     end
   end
 end
